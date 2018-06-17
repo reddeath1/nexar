f33cf9d5fc0a75b8eadc2726229729f1ea6177ec
Frank Galos <frankslayer1@gmail.com>
2018-06-17 12:53:17 +0300

    adding the repository

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const electron  = require('electron')
+const path = require('path')
 const {app, BrowserWindow} = electron
 
 app.on('ready',()=>{
-    let win = new BrowserWindow({width:800,height:600,frame:false,
-        icon: __dirname + '/images/logos/logo.ico'})
-    win.loadURL(`file://${__dirname}/index.html`)
-})
+    let win = new BrowserWindow({
+        width: 1281,
+        height: 800,
+        minWidth: 1281,
+        minHeight: 800,
+        icon: path.join(__dirname, '/images/logos/logo.ico'),
+        titleBarStyle: 'hidden'
+    })
+    
+    //win.loadURL(`file://${__dirname}/index.html`)
+    win.show()
+});
