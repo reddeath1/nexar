11681845f48ca0a7670502829fbe05a53ec1ba8d
reddeath1 <frankslayer1@gmail.com>
2018-06-17 16:41:19 +0300

    disabling the menu bar

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const electron  = require('electron')
+const path = require('path')
 const {app, BrowserWindow} = electron
 
 app.on('ready',()=>{
-    let win = new BrowserWindow({width:800,height:600,frame:false,
-        icon: __dirname + '/images/logos/logo.ico'})
+    let win = new BrowserWindow({
+        width:800,
+        height:600,
+        icon:path.join(__dirname,"/images/logos/logo.ico")})
+    win.setMenu(null)
     win.loadURL(`file://${__dirname}/index.html`)
+    win.show()
 })
