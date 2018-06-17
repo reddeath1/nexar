d67ac5534260aaac38389d4857ef056f61ae7018
Frank Galos <frankslayer1@gmail.com>
2018-06-17 12:57:51 +0300

    adding the repository

--- main.js
+++ main.js
@@ -4,10 +4,8 @@ const {app, BrowserWindow} = electron
 
 app.on('ready',()=>{
     let win = new BrowserWindow({
-        width: 1281,
-        height: 800,
-        minWidth: 800,
-        minHeight: 600,
+        width: 800,
+        height: 600,
         icon: path.join(__dirname, '/images/logos/logo.ico'),
         titleBarStyle: 'hidden'
     })
